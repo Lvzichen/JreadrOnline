b8721e629adcfc57a919bbaaedeb6fa82af07e7d
Administrator <2816665811@qq.com>
2018-08-30 10:01:07 +0800

    Test_all 830

--- app/src/main/java/com/example/administrator/jreadronline/Fragment/BookFragment.java
+++ app/src/main/java/com/example/administrator/jreadronline/Fragment/BookFragment.java
@@ -1,30 +1,64 @@
 package com.example.administrator.jreadronline.Fragment;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.example.administrator.jreadronline.JreaderActivity;
 import com.example.administrator.jreadronline.R;
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
+import com.youth.banner.Transformer;
+import com.youth.banner.listener.OnBannerListener;
+import com.youth.banner.loader.ImageLoader;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class BookFragment extends Fragment {
-
-
-    public BookFragment() {
-        // Required empty public constructor
-    }
-
-
+public class BookFragment extends Fragment implements OnBannerListener {
+    public Banner banner;
+    private ArrayList<String> list_path;
+    private ArrayList<String> list_title;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         View view =  inflater.inflate(R.layout.fragment_book, container, false);
+        banner = view.findViewById(R.id.banner);
+        list_path = new ArrayList<>();
+        list_title = new ArrayList<>();
+
+        list_path.add("https://i04picsos.sogoucdn.com/33857b07b8a385b0");
+        list_path.add("https://i02picsos.sogoucdn.com/b203e74116aa211f");
+        list_path.add("https://i03picsos.sogoucdn.com/0e63c5defcc4d898");
+
+        list_title.add("Android入门到精通");
+        list_title.add("Android面试题");
+        list_title.add("Java入门到精通");
+
+        banner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR_TITLE_INSIDE);
+        banner.setImageLoader(new JreaderActivity.MyLoader());
+        banner.setImages(list_path);
+        banner.setBannerAnimation(Transformer.Default);
+        banner.setBannerTitles(list_title);
+        banner.setDelayTime(2000);
+        banner.isAutoPlay(true);
+        banner.setIndicatorGravity(BannerConfig.CENTER).setOnBannerListener(this).start();
         return view;
     }
 
+    @Override
+    public void OnBannerClick(int position) {
+        Log.i("tag", "你点了第"+position+"张轮播图");
+    }
 }
--- app/src/main/java/com/example/administrator/jreadronline/JreaderActivity.java
+++ app/src/main/java/com/example/administrator/jreadronline/JreaderActivity.java
@@ -5,11 +5,8 @@ import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -19,16 +16,12 @@ import com.example.administrator.jreadronline.Fragment.FaceFragment;
 import com.example.administrator.jreadronline.Fragment.MyFragment;
 import com.example.administrator.jreadronline.Fragment.WordFragment;
 import com.youth.banner.Banner;
-import com.youth.banner.BannerConfig;
-import com.youth.banner.Transformer;
-import com.youth.banner.listener.OnBannerListener;
 import com.youth.banner.loader.ImageLoader;
-import com.youth.banner.loader.ImageLoaderInterface;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class JreaderActivity extends AppCompatActivity implements OnBannerListener, View.OnClickListener {
+public class JreaderActivity extends AppCompatActivity implements View.OnClickListener {
     private TextView title,bookTv,faceTv,meTv,guanyuTv;
     private ViewPager vp;
     private BookFragment bookFragment;
@@ -38,8 +31,6 @@ public class JreaderActivity extends AppCompatActivity implements OnBannerListen
     private List<Fragment> list = new ArrayList<Fragment>();
     private FragmentAdatper adatper;
     private Banner banner;
-    private ArrayList<String> list_path;
-    private ArrayList<String> list_title;
     String[] titles = new String[]{"书籍","面试题目","我的","关于"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -94,34 +85,13 @@ public class JreaderActivity extends AppCompatActivity implements OnBannerListen
         list.add(myFragment);
         list.add(wordFragment);
 
-
-        banner = findViewById(R.id.banner);
-        list_path = new ArrayList<>();
-        list_title = new ArrayList<>();
-
-        list_path.add("https://i04picsos.sogoucdn.com/33857b07b8a385b0");
-        list_path.add("https://i02picsos.sogoucdn.com/b203e74116aa211f");
-        list_path.add("https://i03picsos.sogoucdn.com/0e63c5defcc4d898");
-
-        list_title.add("Android入门到精通");
-        list_title.add("Android面试题");
-        list_title.add("Java入门到精通");
-
-        banner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR_TITLE_INSIDE);
-        banner.setImageLoader(new MyLoader());
-        banner.setImages(list_path);
-        banner.setBannerAnimation(Transformer.Default);
-        banner.setBannerTitles(list_title);
-        banner.setDelayTime(2000);
-        banner.isAutoPlay(true);
-        banner.setIndicatorGravity(BannerConfig.CENTER).setOnBannerListener(this).start();
     }
-
-    @Override
-    public void OnBannerClick(int position) {
-        Log.i("tag","你点了第"+position+"长轮播图");
+    public static class MyLoader extends ImageLoader {
+        @Override
+        public void displayImage(Context context, Object path, ImageView imageView) {
+            Glide.with(context).load((String) path).into(imageView);
+        }
     }
-
     @Override
     public void onClick(View view) {
         switch (view.getId()){
@@ -133,13 +103,6 @@ public class JreaderActivity extends AppCompatActivity implements OnBannerListen
                 break;
             case R.id.item_me:
                 break;
-        }
-    }
-
-    private class MyLoader extends ImageLoader {
-        @Override
-        public void displayImage(Context context, Object path, ImageView imageView) {
-            Glide.with(context).load((String)path).into(imageView);
         }
     }
 }
