3f8d042f42e184df90cf94d36190a559a8b251c4
Administrator <2816665811@qq.com>
2018-08-30 09:22:18 +0800

    Test_all 830

--- app/src/main/java/com/example/administrator/jreadronline/Fragment/WordFragment.java
+++ app/src/main/java/com/example/administrator/jreadronline/Fragment/WordFragment.java
@@ -23,6 +23,7 @@ public class WordFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        //Test
         View view = inflater.inflate(R.layout.fragment_word, container, false);
         return view;
     }
